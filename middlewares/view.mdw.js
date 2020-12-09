87185746278055d8baa9857b062dfc33b1c8ca4a
cdkhiem99 <cdkhiem99@gmail.com>
2020-12-09 22:00:56 +0700

    update view.mdw

--- middlewares/view.mdw.js
+++ middlewares/view.mdw.js
@@ -1,14 +1,15 @@
 const exphbs = require('express-handlebars');
-const hbs_sections = require('express-handlebars-sections');
 const numeral = require('numeral');
 
 module.exports = function (app) {
   app.engine('hbs', exphbs({
-    defaultLayout: 'main.hbs',
+    // defaultLayout: 'main.hbs',
+    defaultLayout: 'bs4.hbs',
     helpers: {
-      section: hbs_sections(),
-      format: val => numeral(val).format('0,0')
+      format_number(val) {
+        return numeral(val).format('0,0');
+      }
     }
   }));
   app.set('view engine', 'hbs');
-};
+}
