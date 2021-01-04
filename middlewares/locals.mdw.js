0d31584b08dd5053c45e09505a85f750a5176c61
Hưng Cao <59307814+HiraiMomo-TWICE@users.noreply.github.com>
2021-01-04 21:27:32 +0700

    Update middlewares and route
    - Add cart route
    - Update locals mdw

--- middlewares/locals.mdw.js
+++ middlewares/locals.mdw.js
@@ -1,13 +1,19 @@
+const cartModel = require('../models/cart.model');
 const categoryModel = require("../models/category.model");
 
 module.exports = function (app) {
   app.use(function (req, res, next) {
-    if (typeof req.session.auth === "undefined") {
+    if (typeof (req.session.auth) === 'undefined') {
       req.session.auth = false;
+    }
+
+    if (req.session.auth === false) {
+      req.session.cart = [];
     }
 
     res.locals.auth = req.session.auth;
     res.locals.authUser = req.session.authUser;
+    res.locals.cartSummary = cartModel.getNumberOfItems(req.session.cart);
     next();
   });
 
