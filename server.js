936e7a7d2ebca0b6ece6922b9a8d8d90697ae1fd
cdkhiem99 <cdkhiem99@gmail.com>
2021-01-10 20:54:43 +0700

    restore global configurations

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require("./startup/logging")();
 require("./startup/config")();
 require("./startup/routes")(app);
 
-const port = 25565;
+const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log(`Server is running at 'http://localhost:${port}'`);
 });
--- startup/config.js
+++ startup/config.js
@@ -3,15 +3,15 @@ const config = require("config");
 module.exports = function () {
   const env_var = "env-var";
   const FATAL_ERROR = `FATAL ERROR: '${env_var}' is not defined.`;
-  // if (!config.get("jwtPrivateKey")) {
-  //   throw new Error("FATAL ERROR: 'jwtPrivateKey' is not defined.");
-  // }
+  if (!config.get("jwtPrivateKey")) {
+    throw new Error("FATAL ERROR: 'jwtPrivateKey' is not defined.");
+  }
   if (!config.get("mysql_port")) {
     throw new Error(FATAL_ERROR.replace(env_var, "mysql.port"));
   }
-  // if (!config.get("mysql_password")) {
-  //   throw new Error(FATAL_ERROR.replace(env_var, "mysql.password"));
-  // }
+  if (!config.get("mysql_password")) {
+    throw new Error(FATAL_ERROR.replace(env_var, "mysql.password"));
+  }
   if (!config.get("mysql_database")) {
     throw new Error(FATAL_ERROR.replace(env_var, "mysql.database"));
   }
