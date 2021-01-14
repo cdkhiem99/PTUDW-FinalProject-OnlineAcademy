e639a69e9d6abb3ba3fe28a4892d4bc8d0e92ef9
cdkhiem99 <cdkhiem99@gmail.com>
2021-01-14 19:41:01 +0700

    3-4 khoa hoc noi bat (successful)

--- routes/home.route.js
+++ routes/home.route.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const courseModel = require("../models/course.model");
+const debug = require("debug")("routes:home");
 
 router.get("/", async function (req, res) {
-  console.log("1");
+  const popularCourses = await courseModel.get4HighlightedCourse();
   const mostViewedCourses = await courseModel.get10mostView();
-  console.log("2");
-  const newViewedCourses = await courseModel.get10LatestCourse();
-  console.log("3");
-  const highlightedCourses = await courseModel.get4HighlightedCourse();
-  console.log("4");
-  res.render("home", {
-    highlightedCourses: highlightedCourses,
-    newViewedCourses: newViewedCourses,
-    mostViewedCourses: mostViewedCourses
-  })
+  const newCourses = await courseModel.get10LatestCourse();
 
+  debug("popularCourses", popularCourses[0]);
+  debug("mostViewedCourses", mostViewedCourses[0]);
+  debug("newCourses", newCourses[0]);
+
+  res.render("home2", {
+    popularCourses: popularCourses,
+    mostViewedCourses: mostViewedCourses,
+    newCourses: newCourses,
+  });
 });
 
 module.exports = router;
