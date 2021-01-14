116ed3bf8250fe541465bf2ec75ad9cd80708315
cdkhiem99 <cdkhiem99@gmail.com>
2021-01-14 09:43:40 +0700

    about to deprecate "home.hbs"

--- routes/home.route.js
+++ routes/home.route.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const courseModel = require("../models/course.model");
 
-router.get("/", function (req, res) {
-  res.render("home");
+router.get("/", async function (req, res) {
+  const mostViewedCourses = await courseModel.get10mostView();
+  res.render("home", {
+    mostViewedCourses: mostViewedCourses,
+  });
 });
 
 module.exports = router;
