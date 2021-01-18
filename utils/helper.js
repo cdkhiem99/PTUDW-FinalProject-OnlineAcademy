4729c9d194f01e74428d4a2d52888ba0d6136b6f
leleeeee <huynhducle20000@gmail.com>
2021-01-18 18:15:07 +0700

    fix enroll button display bug
    - if user is not admin or lecturer will display enroll and watchlist button
    - create `addLecturer` button for admin

--- utils/helper.js
+++ utils/helper.js
@@ -1,5 +1,12 @@
 module.exports = {
     eq(str1, str2){
         return str1.toString() === str2.toString();
+    },
+    cs(str1, str2){
+        if (str1===null)
+            return true;
+        if (str1.toString() === str2.toString())
+            return true;
+        return false;
     }
 };
