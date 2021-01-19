be5b1d0bcdb54f32caec77ec52754a348979d512
cdkhiem99 <cdkhiem99@gmail.com>
2021-01-19 13:06:48 +0700

    sync route:product to route:course

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,45 +1,17 @@
 const express = require("express");
-const { paginate } = require('../config/default.json');
+const router = express.Router();
+
+const { paginate } = require("../config/default.json");
 const courseModel = require("../models/course.model");
 const subfieldModel = require("../models/subfield.model");
 const feedbackModel = require("../models/feedback.model");
 const sectionModel = require("../models/section.model");
-const debug = require("debug")("routes:product");
 const studentModel = require("../models/enroll.model");
 const watchL = require("../models/watchlist.model");
 const lecturerModel = require("../models/lecturer.model");
-const fieldModel = require("../models/fields.model");
 const fieldsModel = require("../models/fields.model");
 
-const router = express.Router();
-
-// router.get("/", async function (req, res, next) {
-//   const list = await courseModel.all();
-//   res.render("vwProducts/index", {
-//     products: list,
-//     empty: list.length === 0,
-//   });
-
-//   const id = await subfieldModel.getBySubName(req.body.fieldsName);
-
-//   const listByFields = await courseModel.getAllCourseByField(id);
-//   res.render("vwProducts/byCats", {
-//     courseByField: listByFields,
-//     empty: listByFields.length === 0,
-//   });
-
-//   // try {
-//   //   const list = await productModel.all();
-//   //   res.render('vwProducts/index', {
-//   //     products: list,
-//   //     empty: list.length === 0
-//   //   });
-//   // } catch (err) {
-//   //   next(err);
-//   //   // console.error(err);
-//   //   // res.send('err');
-//   // }
-// });
+const debug = require("debug")("routes:product");
 
 router.get("/", async function (req, res) {
   const listLecturersName = await lecturerModel.getAllLecturerName();
@@ -56,7 +28,7 @@ router.get("/", async function (req, res) {
   for (i = 1; i <= nPages; i++) {
     page_numbers.push({
       value: i,
-      isCurrentPage: i === +page
+      isCurrentPage: i === +page,
     });
   }
 
@@ -68,14 +40,14 @@ router.get("/", async function (req, res) {
     empty: list.length === 0,
     listLecturersName,
     listFieldsName,
-    page_numbers
-  })
+    page_numbers,
+  });
 });
 
 router.get("/courseBySubField/:subField", async function (req, res, next) {
   const id = await subfieldModel.getBySubName(req.params.subField);
 
-  if (id === null){
+  if (id === null) {
     res.redirect("/");
     return;
   }
@@ -100,12 +72,15 @@ router.get("/field/:Field", async function (req, res, next) {
   for (i = 1; i <= nPages; i++) {
     page_numbers.push({
       value: i,
-      isCurrentPage: i === +page
+      isCurrentPage: i === +page,
     });
   }
 
   const offset = (page - 1) * paginate.limit;
-  const listByFields = await courseModel.getAllCourseByField(req.params.Field, offset);
+  const listByFields = await courseModel.getAllCourseByField(
+    req.params.Field,
+    offset
+  );
   res.locals.listByFields = listByFields;
   res.locals.page_numbers = page_numbers;
   res.locals.empty = listByFields === 0;
@@ -114,65 +89,76 @@ router.get("/field/:Field", async function (req, res, next) {
 
 router.get("/detail/:courseID", async function (req, res, next) {
   const course = await courseModel.getCourseByID(req.params.courseID);
-  
+
   const feedbackList = await feedbackModel.getFeedBack(req.params.courseID);
 
-  var courseContent;
-  var isFisnish;
-  if(res.locals.auth===false){
-    courseContent = await sectionModel.getCourseContent(
-      req.params.courseID
-    );
-  } else{
+  let courseContent;
+  let isFisnish;
+  if (res.locals.auth === false) {
+    courseContent = await sectionModel.getCourseContent(req.params.courseID);
+  } else {
     courseContent = await sectionModel.getCourseContentWithProcess(
-      req.params.courseID,res.locals.authUser.id
+      req.params.courseID,
+      res.locals.authUser.id
     );
 
-    var count=0;
+    let count = 0;
     for (const key in courseContent) {
       if (Object.hasOwnProperty.call(courseContent, key)) {
         const element = courseContent[key];
-        if (element.isComplete===true){
-          count+=1;
+        if (element.isComplete === true) {
+          count += 1;
         }
       }
     }
 
-    if(count===courseContent.length){
-      isFisnish=true;
-    }
-    else{
-      isFisnish=false;
+    if (count === courseContent.length) {
+      isFisnish = true;
+    } else {
+      isFisnish = false;
     }
   }
 
+  let panelID = 2;
+  let paneltitleID = 3;
+  let firstPreview = "javascript:;";
+  for (const section_ of courseContent) {
+    section_.panelID = panelID;
+    section_.paneltitleID = paneltitleID;
+
+    panelID += 4;
+    paneltitleID += 4;
+
+    if (section_.preview && firstPreview === "javascript:;") {
+      firstPreview = section_.preview;
+      debug("firstPreview: ", firstPreview);
+    }
+  }
+  course.CourseIntro = firstPreview;
 
   let isEnrolled = false;
-  if(res.locals.auth && res.locals.authUser.role === 'student'){
-    isEnrolled = await studentModel.isEnroll(res.locals.authUser.id, course.CourseID);
+  if (res.locals.auth && res.locals.authUser.role === "student") {
+    isEnrolled = await studentModel.isEnroll(
+      res.locals.authUser.id,
+      course.CourseID
+    );
   }
 
   let isInWatchList = false;
-  if(res.locals.auth && res.locals.authUser.role === 'student'){
-    isInWatchList = await watchL.isInWatchList(res.locals.authUser.id, course.CourseID);
+  if (res.locals.auth && res.locals.authUser.role === "student") {
+    isInWatchList = await watchL.isInWatchList(
+      res.locals.authUser.id,
+      course.CourseID
+    );
   }
 
-  // res.locals.course = course;
-  // res.locals.empty = course === 0;
-  // res.locals.feedbackList = feedbackList;
-  // res.locals.feedbackempty = feedbackList === 0;
-  // res.locals.previewVideo = previewVideo;
-  // res.locals.noPreviewVideo = previewVideo === 0;
-  // res.locals.courseContent = courseContent;
-  // res.locals.courseContentempty = courseContent === 0;
-
-  res.render("vwProducts/detail", {
+  res.render("vwCourses/details", {
     course,
     feedbackList,
     courseContent,
     isEnrolled,
     isInWatchList,
-    isFisnish
+    isFisnish,
   });
 });
 
@@ -191,7 +177,7 @@ router.get("/search/:searchStr", async function (req, res, next) {
   for (i = 1; i <= nPages; i++) {
     page_numbers.push({
       value: i,
-      isCurrentPage: i === +page
+      isCurrentPage: i === +page,
     });
   }
 
